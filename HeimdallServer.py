f1da5f2b386d33e2ee8641f2a793af03bcdde51d
Luis Zuniga <luisangel.cpp@gmail.com>
2016-06-17 20:25:03 -0700

    Avoid using a specific IP on the listener. This allows to listen to messages sent to any of ifconfig/ip addr.

--- HeimdallServer.py
+++ HeimdallServer.py
@@ -14,19 +14,19 @@ PASSCODE_SIZE = 4
 HARDCODED_PASSWORD = b"1234"
 MSG_SUCCESS = b"SUCCESS"
 MSG_FAIL    = b"FAIL"
+SERVER_PORT = ("", 1333)
 
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG, format='[%(asctime)s] [%(levelname)s] : %(message)s')
 
-server_port = ('localhost', 1234)
-logging.debug("Starting on %s:%s" % server_port)
+logging.debug("Starting on %s:%s" % SERVER_PORT)
 
 while True:
     # Wait for a connection
     logging.debug("Waiting for a passcode...")
     while True:
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        sock.bind(server_port)
-        passcode, addr = sock.recvfrom(1024)
+        sock.bind(SERVER_PORT)
+        passcode, addr = sock.recvfrom(PASSCODE_SIZE)
         logging.debug("Connected by %s", addr)
         logging.debug("Received passcode %s", passcode)
         if not passcode: break
